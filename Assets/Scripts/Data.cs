a1778ae8e6882322e92ec53e3e9d4cf21394d566
Рябчиков Олег <o.ryabchikov@crazypanda.ru>
2020-04-17 21:20:51 +0300

    template project

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour
+{
+    public Player PlayerSO;
+    public Consts ConstsSO;
+
+    public static Player player
+    {
+        get
+        {
+            if (instance)
+                return instance.PlayerSO;
+            else
+            {
+                instance = FindObjectOfType<Data>();
+                return instance.PlayerSO;
+            }
+        }
+        private set => instance.PlayerSO = value;
+    }            
+    public static Consts consts
+    {
+        get
+        {
+            if (instance)
+                return instance.ConstsSO;
+            else
+            {
+                instance = FindObjectOfType<Data>();
+                return instance.ConstsSO;
+            }
+        }
+        private set => instance.ConstsSO = value;
+    }   
+
+    public static Data instance;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SO/Consts.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SOs/Consts", fileName = "Consts")]
+public class Consts : ScriptableObject
+{
+
+}
