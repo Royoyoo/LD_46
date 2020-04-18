6b4bbc3de2c9d1181a8c49c5dee846b585012ca1
andrey30353 <andrey30353@mail.ru>
2020-04-18 15:59:21 +0300

    Добавил спавнер душ. После спавна они двигаются в точку

---
+++ Assets/Scripts/Soul.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soul : MonoBehaviour
+{
+    [Range(0.1f, 2f)]
+    public float Speed = 1;
+    [Range(0.1f, 2f)]
+    public float MovingTime = 1;
+    
+   
+    private float startTime;
+    private float journeyLength;
+
+    public Vector3 startPosition;
+    public Vector3 Destination;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startPosition, Destination);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distCovered = (Time.time - startTime) * Speed;
+        float fracJourney = distCovered / journeyLength;
+        transform.position = Vector3.Lerp(startPosition, Destination, fracJourney);
+
+        //transform.localPosition = Vector3.Lerp(startPosition, Destination, Time.deltaTime / MovingTime);
+
+        // поворот к центру 
+        //transform.localRotation = 
+    }
+}
---
+++ Assets/Scripts/SoulsSpawner.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+
+public class SoulsSpawner : MonoBehaviour
+{
+    [Range(0.1f, 3f)]
+    public float AreaRadius = 1;
+
+    public int SoulsCount = 0;
+
+    public int MaxVisibleSoulsCount = 100;
+
+    public Transform soulDestinationMarker;
+
+    public event Action<int> OnSoulSpawn;
+
+    public Soul SoulPrefab;
+
+    // В секундах
+    public float SpawnDelay = 1f;
+
+    private float currentProgress = 0;
+
+
+    private void Update()
+    {
+        currentProgress += Time.deltaTime;
+        if(currentProgress > SpawnDelay)
+        {
+            currentProgress -= SpawnDelay;
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        Debug.Log("Spawn");
+        SoulsCount++;
+
+        if(SoulsCount < MaxVisibleSoulsCount)
+        {
+            var newSoul = Instantiate(SoulPrefab, this.transform);
+            var randomPoint = UnityEngine.Random.insideUnitSphere;
+            randomPoint.y = 0;
+
+            newSoul.Destination = soulDestinationMarker.position + randomPoint * AreaRadius;
+        }
+
+        OnSoulSpawn?.Invoke(SoulsCount);        
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Место спавна
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, 0.5f);
+
+        //Пункт назначения
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(soulDestinationMarker.position, AreaRadius);
+    }
+}
