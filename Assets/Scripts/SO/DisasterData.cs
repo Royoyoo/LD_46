34bff6349d14318b98bb83a6e2bd3aa36df6cdb3
andrey30353 <andrey30353@mail.ru>
2020-04-19 17:27:09 +0300

    Добавил данные для 3 бедствий

---
+++ Assets/Scripts/SO/DisasterData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "SOs/DisasterData", fileName = "DisasterData")]
+public class DisasterData : ScriptableObject
+{
+    public string Name;
+
+    public Sprite Icon;
+
+    [Header("Процент от всего количества")]
+    [Range(0.05f, 0.5f)]
+    public float MinusSoulsPercent;
+
+    [Header("Затем вычитание этого числа")] 
+    [Range(1f, 10000f)]
+    public int MinusSoulsCount;  
+}
