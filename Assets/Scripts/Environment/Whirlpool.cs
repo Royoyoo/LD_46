d1c96968761922c6234582a0014457edcb481041
andrey30353 <andrey30353@mail.ru>
2020-04-28 21:32:28 +0300

    Добавил водовороты

---
+++ Assets/Scripts/Environment/Whirlpool.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Whirlpool : MonoBehaviour
+{   
+    [Header("Сила в прямоугольнике")]
+    [Range(1f, 1000f)]
+    [SerializeField] float rectangleForce;
+
+    [Header("Сила в сфере")]
+    [Range(1f, 1000f)]
+    [SerializeField] float sphereForce;  
+    [Range(1f, 50f)]
+    [SerializeField] float radiusSphereForce;
+
+    private void OnTriggerStay(Collider other)
+    {
+        var rb = other.GetComponent<Rigidbody>();
+        if (rb == null)
+            return;
+
+        var direction = (transform.position - other.transform.position);
+        // Debug.Log("direction " + other.name + " " + direction);           
+        // Debug.Log(other.name + " " + force);
+        rb.AddForce(direction.normalized * rectangleForce, ForceMode.Force);
+
+        // если в радиусе сферы, то добавляем силу
+        var sqrDistance = direction.sqrMagnitude;
+        if (sqrDistance < radiusSphereForce * radiusSphereForce)
+        {
+            rb.AddForce(direction.normalized * sphereForce, ForceMode.Force);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radiusSphereForce);
+
+    }
+}
---
+++ Assets/Scripts/Environment/WhirlpoolSpawner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WhirlpoolSpawner : MonoBehaviour
+{
+    [Range(1, 60)]
+    [SerializeField] private int Timeout;
+
+    [Range(1, 60)]
+    [SerializeField] private int TimeOfAction;
+
+    Whirlpool[] whirlpools;
+
+    private void Awake()
+    {
+        whirlpools = GetComponentsInChildren<Whirlpool>(true);
+    }
+
+    void Start()
+    {     
+        StartCoroutine(GameLoop());
+    }
+
+    
+    private IEnumerator GameLoop()
+    {
+        yield return new WaitForSeconds(Data.consts.StartDelay);
+
+        while (true)
+        {
+            yield return new WaitForSeconds(Timeout);            
+            SpawnAll();
+            yield return new WaitForSeconds(TimeOfAction);
+            HideAll();
+        }
+    }
+
+    [ContextMenu("SpawnAll")]
+    private void SpawnAll()
+    {
+        for (int i = 0; i < whirlpools.Length; i++)
+        {
+            whirlpools[i].gameObject.SetActive(true);
+        }
+    }
+
+    [ContextMenu("HideAll")]
+    private void HideAll()
+    {
+        for (int i = 0; i < whirlpools.Length; i++)
+        {
+             whirlpools[i].gameObject.SetActive(false);
+        }
+    }
+}
